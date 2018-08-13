a3384551df093d580c27f8f24544a539444e1254
Tringapps-Admin <tringapps-admin@anugheerthi.local>
2018-08-13 12:25:39 +0530

    Initial Commit

---
+++ UniversalTextStyle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UniversalTextStyle
+//
+//  Created by Tringapps-Admin on 13/08/18.
+//  Copyright © 2018 Tringapps-Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
